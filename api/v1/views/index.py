afaa109e9a79ce4c06612221ca5b5077356885f4
tykoon787 <tykoon787@protonmail.com>
2023-05-08 12:25:06 +0300

    Changed codebase

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
 """
-Index page of the application
-
-Returns:
-    JSON : Status of the server
+Flask route that returns json status response
 """
-
-from flask import jsonify
 from api.v1.views import app_views
+from flask import jsonify, request
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
-def get_status():
+def status():
     """
-    Function to get the status
+    function for status route that returns the status
     """
-    return jsonify({'status': 'OK'})
+    if request.method == 'GET':
+        resp = {"status": "OK"}
+        return jsonify(resp)
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    function to return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
