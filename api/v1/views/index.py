85bfbaade5c5c442318d756b694402d73adf7547
tykoon787 <tykoon787@protonmail.com>
2023-05-08 14:49:46 +0300

    Updated Code

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,28 +12,29 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """Returns the status
+if __name__ == "__main__":
+    @app_views.route('/status', methods=['GET'], strict_slashes=False)
+    def status():
+        """Returns the status
 
-    Returns:
-        JSON: Status in JSON Format
-    """
-    return jsonify({'status': 'OK'})
+        Returns:
+            JSON: Status in JSON Format
+        """
+        return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats():
-    """Returns the count of all class objects"""
-    objects: dict = {}
-    classes: dict = {
-        "Amenity": "amenities",
-        "City": "cities",
-        "Place": "places",
-        "Review": "reviews",
-        "State": "states",
-        "User": "users"
-    }
-    for key, value in classes.items():
-        objects[value] = storage.count(key)
-    return jsonify(objects)
+    @app_views.route('/stats', methods=['GET'], strict_slashes=False)
+    def stats():
+        """Returns the count of all class objects"""
+        objects: dict = {}
+        classes: dict = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in classes.items():
+            objects[value] = storage.count(key)
+        return jsonify(objects)
