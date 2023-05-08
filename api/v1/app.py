db493e89ad0e3d2a7486dc66fbac66a2b6a50a17
tykoon787 <tykoon787@protonmail.com>
2023-05-08 10:12:37 +0300

    updated py code

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,12 +13,14 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown():
     """
     Teardown
     """
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, threaded=True)
